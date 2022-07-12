80ec6fbd2a176a6e429d40f1717443115d8e4f84
Roman Fedoseev <Fedoseev.Roman@gmail.com>
2022-07-11 23:51:26 -0400

    add jest env

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,6 +3,7 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
+    jest: true,
   },
   extends: [
     'airbnb',
@@ -22,7 +23,7 @@ module.exports = {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['react', '@typescript-eslint', 'import'],
+  plugins: ['react', '@typescript-eslint', 'import', 'prettier'],
   rules: {
     'prettier/prettier': ['error', { endOfLine: 'auto' }],
 
